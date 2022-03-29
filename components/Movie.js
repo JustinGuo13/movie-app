a0d57f0aa435ac8485f97ea8aed02b2687c24c8d
Peng Yang Guo <pyangguo@gmail.com>
2022-03-29 15:55:30 -0700

    Implement Movie component

--- components/Movie.js
+++ components/Movie.js
@@ -1,4 +1,26 @@
-function Movie() {
-	return <div>This is a movie</div>;
-}
+import Image from 'next/image';
+
+const IMAGE_API = 'https://image.tmdb.org/t/p/w1280';
+const Movie = ({ title, poster_path, overview, vote_average }) => {
+	return (
+		// Container
+		<div className="relative m-4 bg-gray-900 rounded-lg shadow-md shadow-black overflow-hidden w-[18.75rem] z-0 ">
+			<div className="max-w-full">
+				<Image src={IMAGE_API + poster_path} width={300} height={400} alt={title} />
+			</div>
+
+			{/* Movie Info */}
+			<div className="flex p-4 items-center justify-between ">
+				<h3>{title}</h3>
+				<span>{vote_average}</span>
+			</div>
+
+			{/* Overview */}
+			<div className="bg-gray-900 absolute bottom-0 left-0 right-0 p-4 transform translate-y-[100%] hover:translate-y-[0%] ease-out duration-500 z-10">
+				<h1>Overview:</h1>
+				<p>{overview}</p>
+			</div>
+		</div>
+	);
+};
 export default Movie;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next';
+import { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
 
 const Home: NextPage = () => {
-	const FEATURED_API = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.THE_MOVIE_DB_API_KEY}&language=en-US&page=1`;
+	const MOVIE_API = process.env.NEXT_PUBLIC_MOVIE_DB_API_KEY;
+	const FEATURED_API = `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIE_API}&language=en-US&page=1`;
 
-	console.log(FEATURED_API);
 	// const IMAGE_API =
 	// const SEARCH_API=
 	const [movies, setMovies] = useState([]);
@@ -14,11 +14,15 @@ const Home: NextPage = () => {
 		fetch(FEATURED_API)
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data);
+				// console.log(data);
 				setMovies(data.results);
 			});
 	});
-	return <div>{movies.length > 0 && movies.map((movie) => <Movie />)}</div>;
+	return (
+		<div className="flex flex-wrap bg-gray-900 text-yellow-300">
+			{movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+		</div>
+	);
 };
 
 export default Home;
