0d33014674e834f32a2dcc20e91508dbee053837
Peng Yang Guo <pyangguo@gmail.com>
2022-03-30 13:20:51 -0700

    Implement Search bar

--- components/Movie.js
+++ components/Movie.js
@@ -14,12 +14,6 @@ const Movie = ({ title, poster_path, overview, vote_average }) => {
 				<h3>{title}</h3>
 				<span>{vote_average}</span>
 			</div>
-
-			{/* Overview */}
-			<div className="bg-gray-900 absolute bottom-0 left-0 right-0 p-4 transform translate-y-[100%] hover:translate-y-[0%] ease-out duration-500 z-10">
-				<h1>Overview:</h1>
-				<p>{overview}</p>
-			</div>
 		</div>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,9 @@ const Home: NextPage = () => {
 
 	// const IMAGE_API =
 	// const SEARCH_API=
+	const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API}&language=en-US&page=1&query=`;
 	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
 
 	useEffect(() => {
 		fetch(FEATURED_API)
@@ -17,11 +19,41 @@ const Home: NextPage = () => {
 				// console.log(data);
 				setMovies(data.results);
 			});
-	});
+
+		return () => {
+			// Cleaning up a subscription
+			setMovies([]);
+		};
+	}, []);
+
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+
+		fetch(SEARCH_API + searchTerm)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setMovies(data.results);
+			});
+	};
+
 	return (
-		<div className="flex flex-wrap bg-gray-900 text-yellow-300">
-			{movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
-		</div>
+		<>
+			<header className="bg-gray-800 p-4">
+				<form onSubmit={handleOnSubmit}>
+					<input
+						className="bg-gray-900 rounded-lg p-2 text-yellow-300 focus:outline-none focus:border-yellow-300 focus:ring-1 focus:ring-yellow-300"
+						type="search"
+						placeholder="Search Movies"
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+				</form>
+			</header>
+			<div className="flex flex-wrap bg-gray-900 text-yellow-300">
+				{movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+			</div>
+		</>
 	);
 };
 
