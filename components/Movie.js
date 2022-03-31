4601014ffaa066fa205c9cbda3dacc67ac9454cb
Peng Yang Guo <pyangguo@gmail.com>
2022-03-31 15:42:16 -0700

    Implement pagination button

--- components/Movie.js
+++ components/Movie.js
@@ -1,30 +1,36 @@
 import Image from 'next/image';
+import placeHolder from '../image/placeholder.png';
 
-const IMAGE_API = 'https://image.tmdb.org/t/p/w1280';
-const setVote = (vote) => {
+const IMAGE_API = 'https://image.tmdb.org/t/p/original';
+
+const setVoteColor = (vote) => {
 	if (vote >= 8) {
-		return 'text-green-300';
+		return 'text-green-500 bg-gray-800 p-1 rounded font-bold';
 	} else if (vote >= 6) {
-		return 'text-orange-300';
+		return 'text-orange-500 bg-gray-800 p-1 rounded font-bold';
 	} else {
-		return 'text-red-300';
+		return 'text-red-500 bg-gray-800 p-1 rounded font-bold';
 	}
 };
+
 const Movie = ({ title, poster_path, overview, vote_average }) => {
 	return (
 		// Container
-		<div className="relative m-4 bg-gray-900 rounded-lg shadow-md shadow-black overflow-hidden w-[18.75rem] z-0 ">
-			<div className="max-w-full">
-				<Image src={IMAGE_API + poster_path} width={300} height={400} alt={title} />
+		<div className="relative m-4 bg-gray-900 rounded-lg shadow-md shadow-black overflow-hidden w-[18.75rem] z-0 hover:opacity-50">
+			<div className="w-full">
+				<Image
+					src={poster_path ? IMAGE_API + poster_path : placeHolder}
+					width={300}
+					height={400}
+					objectFit="cover"
+					alt={title}
+				/>
 			</div>
 
 			{/* Movie Info */}
 			<div className="flex p-4 items-center justify-between ">
-				<h3>{title}</h3>
-				{/* FIXME: Change vote colors  */}
-				<span className={`$setVote(vote_average) bg-gray-800 p-1 rounded font-bold`}>
-					{vote_average}
-				</span>
+				<h3 className="text-lg font-bold">{title}</h3>
+				<span className={setVoteColor(vote_average)}>{vote_average}</span>
 			</div>
 		</div>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/solid';
 
 const Home: NextPage = () => {
-	const MOVIE_API = process.env.NEXT_PUBLIC_MOVIE_DB_API_KEY;
-	const FEATURED_API = `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIE_API}&language=en-US&page=1`;
-
-	// const IMAGE_API =
-
-	const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API}&language=en-US&page=1&query=`;
 	const [movies, setMovies] = useState<any[]>([]);
 	const [searchTerm, setSearchTerm] = useState('');
 	const [error, setError] = useState(null);
+	const [page, setPage] = useState(1);
+
+	const MOVIE_API = process.env.NEXT_PUBLIC_MOVIE_DB_API_KEY;
+	const FEATURED_API = `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIE_API}&language=en-US&page=${page}`;
+	const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API}&language=en-US&page=1&query=`;
 
 	useEffect(() => {
 		getMovies(FEATURED_API);
@@ -26,11 +26,11 @@ const Home: NextPage = () => {
 				// console.log(data);
 				setMovies(data.results);
 			})
-			.catch((err) => {
-				if (err.name === 'AbortError') {
+			.catch((error) => {
+				if (error.name === 'AbortError') {
 					console.log('successfully aborted');
 				} else {
-					setError(err);
+					setError(error);
 				}
 			});
 		return () => controller.abort();
@@ -45,21 +45,47 @@ const Home: NextPage = () => {
 		}
 	};
 
+	const previousPage = () => {
+		if (page > 1) {
+			setPage(page - 1);
+			getMovies(FEATURED_API);
+		}
+	};
+
+	const nextPage = () => {
+		setPage(page + 1);
+		getMovies(FEATURED_API);
+	};
+
 	return (
 		<>
-			<header className="flex justify-center bg-gray-800 p-4">
-				<form onSubmit={handleOnSubmit}>
-					<input
-						className="bg-gray-900 rounded-lg p-2 text-yellow-300 focus:outline-none focus:border-yellow-300 focus:ring-1 focus:ring-yellow-300"
-						type="search"
-						placeholder="Search Movies"
-						value={searchTerm}
-						onChange={(e) => setSearchTerm(e.target.value)}
-					/>
-				</form>
-			</header>
-			<div className="flex flex-wrap bg-gray-900 text-yellow-300">
-				{movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+			<div className="bg-gray-900 h-screen  overflow-y-scroll scrollbar-hide">
+				<header className="flex justify-center bg-gray-800 p-4">
+					<form onSubmit={handleOnSubmit}>
+						<input
+							className="bg-gray-900 rounded-lg p-2 text-yellow-300 focus:outline-none focus:border-yellow-300 focus:ring-1 focus:ring-yellow-300"
+							type="search"
+							placeholder="Search Movies"
+							value={searchTerm}
+							onChange={(e) => setSearchTerm(e.target.value)}
+						/>
+					</form>
+				</header>
+				<div className="flex flex-wrap justify-center  text-yellow-300">
+					{movies.length > 0 &&
+						movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+				</div>
+
+				<div className="flex items-center justify-center space-x-2 text-yellow-300">
+					<button onClick={previousPage}>
+						<ChevronDoubleLeftIcon className="h-10 w-10 text-yellow-300" />
+						<p>{page}</p>
+					</button>
+					<button onClick={nextPage}>
+						<ChevronDoubleRightIcon className="h-10 w-10 text-yellow-300" />
+						<p>{page + 1}</p>
+					</button>
+				</div>
 			</div>
 		</>
 	);
